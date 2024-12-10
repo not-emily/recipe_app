0c32ceeaf0194260439db568f4f49c48ffecbb00
Emily <emily@pxp200.com>
2024-12-10 11:36:58 -0700

    Set up landing header, update asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
+//= link_tree ../images/landing
+//= link_tree ../images/branding
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
